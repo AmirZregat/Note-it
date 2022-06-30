69bade8b621ce1971fc3db92059b2d2f68afc176
AmirZregat <ameerzregat97@gmail.com>
2022-07-01 02:26:07 +0300

    nav added

--- app/src/main/java/com/project/noteit/MainActivity.kt
+++ app/src/main/java/com/project/noteit/MainActivity.kt
@@ -2,10 +2,15 @@ package com.project.noteit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+     //git checkout -b nav_component
     }
 }
